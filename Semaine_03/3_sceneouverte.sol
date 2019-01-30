f6fb277f5576e0aca7085007eaefeef6d79e7fc3
Abdou <abdoudicko@hotmail.com>
2019-01-30 23:34:33 +0100

    Semaine 3 administrateurs

---
+++ Semaine_03/3_sceneouverte.sol
@@ -0,0 +1,28 @@
+//Write your own contracts here. Currently compiles using solc v0.4.15+commit.bbb8e64f.
+pragma solidity ^0.4.25;
+contract SceneOuverte {
+  string[12] passagesArtistes;
+  uint creneauxLibres = 12;
+  uint tour;
+
+  function sInscrire(string memory nomArtiste) public {
+    if (creneauxLibres > 0) {
+      passagesArtistes[12-creneauxLibres] = nomArtiste;
+      creneauxLibres--;
+    }
+  }
+
+  function passerArtisteSuivant() public return (string) {
+    if (tour<12) {
+      tour++;
+      return artisteEnCours();
+    } else {
+      return "FIN";
+    }
+  }
+
+  function artisteEnCours() public constant returns (string) {
+    return passagesArtistes[tour];
+  }
+ 
+}
